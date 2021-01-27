e318db818dffd83f40e00c41713bc3ee99c749aa
joanemili <joanemili@gmail.com>
2021-01-27 10:11:49 +0100

    afegit random per les consultes

--- ruleta_c/views.py
+++ ruleta_c/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Consultes, Respostes, Bases, Solucions
+import random
 
 def query_roulette(request):
-    return render(request, 'ruleta_c/query_roulette.html',{})
+    #rang = xrange(10)
+    numero = random.randint(1,10)
+    consulta = Consultes.objects.filter(codi=numero) 
+    return render(request, 'ruleta_c/query_roulette.html',{'consulta':consulta})
