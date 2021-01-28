1f9e04143c0cdeb4a73bb1b52c2d0d70fcb7a56f
joanemili <joanemili@gmail.com>
2021-01-28 20:17:53 +0100

    random a 35

--- ruleta_c/views.py
+++ ruleta_c/views.py
@@ -4,6 +4,6 @@ import random
 
 def query_roulette(request):
     #rang = xrange(10)
-    numero = random.randint(1,10)
+    numero = random.randint(1,35)
     consulta = Consultes.objects.filter(codi=numero) 
     return render(request, 'ruleta_c/query_roulette.html',{'consulta':consulta})
